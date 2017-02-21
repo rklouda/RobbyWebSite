ed25be31dacbc6a8be0b760b4d689322a3847c92
Rob Klouda <rgklouda@comcast.net>
2017-02-21 06:11:21 +0000

    all

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
       build: {
         src: 'src/factorial.js',
         dest: 'build/factorial.min.js'
-      }
+      },
       dist: {
       files: {
         'index.html': ['index.html']
